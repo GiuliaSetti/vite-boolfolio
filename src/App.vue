d5daac58bb1a4d764bbde34073988aba0d494c18
GiuliaSetti <giulia.setti96@gmail.com>
2023-05-31 16:06:42 +0200

    feat: header and started project list

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <script>
+//header
+  import AppHeader from './components/AppHeader.vue';
+//main
   import AppMain from './components/AppMain.vue';
+
   export default {
     name: 'App',
     data() {
     },
     components: {
+      AppHeader,
       AppMain,
     }
   }
 </script>
 
 <template>
+  <AppHeader></AppHeader>
   <router-view></router-view>
 </template>
 
